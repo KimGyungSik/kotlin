42d50820e96461065464db2908d534871966de08
KimGyungSik <0711kyungh@naver.com>
2024-07-03 20:39:14 +0900

    readme update

--- src/main/kotlin/ch8/주 생성자 프로퍼티 이용.kt
+++ src/main/kotlin/ch8/주 생성자 프로퍼티 이용.kt
@@ -1,15 +1,15 @@
 package ch8
 
 class User3(name: String) {
-    var name: String = name
+    val name: String = name
         get() = field.toUpperCase()
-        set(value) {
-            field = "Hello"+value
-        }
+//        set(value) {
+//            field = "Hello"+value
+//        }
 }
 
 fun main(args: Array<String>) {
     val user3 = User3("kkang")
-    user3.name = "kim"
+//    user3.name = "kim"
     println(user3.name)
 }
